c1f2c732a0065daacd82bc429990aea021245e80
meghanathul <92531334+meghanathul@users.noreply.github.com>
2022-03-21 20:35:35 +0530

    js3

---
+++ script.js
@@ -0,0 +1,39 @@
+// var x=5;
+// var y=10;
+// var z=y+x;
+// var z=y-x;
+// var z=y*x;
+// var z=y/x;
+// var z=y%x;  //reminder is output
+// console.log(z);
+
+
+// var x=5;
+// console.log(x++) //post increment=5
+// console.log(x)    //=6
+// console.log(x--) //
+
+// var a=5,b=10,c='5'
+// console.log(a==c)  //true
+// console.log(a===c)  //false
+// console.log(a!=b)  //true
+// console.log(a>b)  //false
+// console.log(a<b)   //true
+// console.log(a>=b)    //false
+// console.log(a<=b)   //true
+
+
+// var a=5;
+// var b=10;
+// console.log((a!=b) && (a<b))  //true
+// console.log((a!=b) || (a>b))  //true
+
+// var a=10,b=5;
+// var c=a>b ? a:b;
+// console.log(c)  //10
+
+var a=3;
+var b=2;
+// &
+// 1101
+// 1100
